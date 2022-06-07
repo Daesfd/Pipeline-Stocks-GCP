92c8edeca29167c164eeb0fbb62208258842349c
edson <edsonicizojunior@gmail.com>
2022-06-07 20:27:50 -0300

    Remoção das transformações, já que não estavam sendo lidas

--- airflow/dags/pipeline.py
+++ airflow/dags/pipeline.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
@@ -22,16 +23,10 @@ def download_data(symbol, output_file):
     return prices.to_csv(output_file)
 
 
-def basic_transformation(input_file, output_file):
-    df = pd.read_csv(input_file)
-    df['Date'] = pd.to_datetime(df['Date'])
-    df['Open'] = df['Open'].astype('float32')
-    df['High'] = df['High'].astype('float32')
-    df['Low'] = df['Low'].astype('float32')
-    df['Close'] = df['Close'].astype('float32')
-    df['Adj Close'] = df['Adj Close'].astype('float32')
-    df['Volume'] = df['Volume'].astype('int32')
-
+def format_to_parquet(input_file, output_file):
+    if not input_file.endswith('.csv'):
+        logging.error("Can only accept source files in CSV format, for the moment")
+        return
     table = pv.read_csv(input_file)
     pq.write_table(table, output_file)
 
@@ -55,7 +50,7 @@ def donwload_parquetize_upload_dag(
         dag,
         stock_name,
         local_csv_path_template,
-        local_parquet_path_template_modfied,
+        local_parquet_path_template,
         tier1_gcs_path_template,
         tier2_gcs_path_template
 ):
@@ -79,13 +74,13 @@ def donwload_parquetize_upload_dag(
             },
         )
 
-        basic_transformation_task = PythonOperator(
-            task_id="basic_transformation_task",
-            python_callable=basic_transformation,
+        format_to_parquet_task = PythonOperator(
+            task_id="format_to_parquet_task",
+            python_callable=format_to_parquet,
             op_kwargs={
                 "symbol": stock_name,
                 "input_file": local_csv_path_template,
-                "output_file": local_parquet_path_template_modfied,
+                "output_file": local_parquet_path_template,
             }
         )
 
@@ -95,20 +90,20 @@ def donwload_parquetize_upload_dag(
             op_kwargs={
                 "bucket": BUCKET,
                 "object_name": tier2_gcs_path_template,
-                "input_file": local_parquet_path_template_modfied,
+                "input_file": local_parquet_path_template,
             },
         )
 
         rm_task = BashOperator(
             task_id="rm_task",
-            bash_command=f"rm {local_csv_path_template} {local_parquet_path_template_modfied}"
+            bash_command=f"rm {local_csv_path_template} {local_parquet_path_template}"
         )
 
-        download_dataset_task >> local_to_gcs_tier1_task >> basic_transformation_task >> local_to_gcs_tier2_task >> rm_task
+        download_dataset_task >> local_to_gcs_tier1_task >> format_to_parquet_task >> local_to_gcs_tier2_task >> rm_task
 
 
 TSLA_CSV_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_TSLA.csv'
-TSLA_PARQUET_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_TSLA_modified.parquet'
+TSLA_PARQUET_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_TSLA.parquet'
 TSLA_GCS_PATH_TEMPLATE_TIER1 = "tier1/TESLA/data_TSLA.csv"
 TSLA_GCS_PATH_TEMPLATE_TIER2 = "tier2/TESLA/data_TSLA.parquet"
 
@@ -126,14 +121,14 @@ donwload_parquetize_upload_dag(
     dag=Tesla_data_dag,
     stock_name="TSLA",
     local_csv_path_template=TSLA_CSV_LOCAL_FILE_TEMPLATE,
-    local_parquet_path_template_modfied=TSLA_PARQUET_LOCAL_FILE_TEMPLATE,
+    local_parquet_path_template=TSLA_PARQUET_LOCAL_FILE_TEMPLATE,
     tier1_gcs_path_template=TSLA_GCS_PATH_TEMPLATE_TIER1,
     tier2_gcs_path_template=TSLA_GCS_PATH_TEMPLATE_TIER2
 )
 
 
 FB_CSV_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_FB.csv'
-FB_PARQUET_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_FB_modified.parquet'
+FB_PARQUET_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_FB.parquet'
 FB_GCS_PATH_TEMPLATE_TIER1 = "tier1/FB/data_FB.csv"
 FB_GCS_PATH_TEMPLATE_TIER2 = "tier2/FB/data_FB.parquet"
 
@@ -151,13 +146,13 @@ donwload_parquetize_upload_dag(
     dag=FB_data_dag,
     stock_name="FB",
     local_csv_path_template=FB_CSV_LOCAL_FILE_TEMPLATE,
-    local_parquet_path_template_modfied=FB_PARQUET_LOCAL_FILE_TEMPLATE,
+    local_parquet_path_template=FB_PARQUET_LOCAL_FILE_TEMPLATE,
     tier1_gcs_path_template=FB_GCS_PATH_TEMPLATE_TIER1,
     tier2_gcs_path_template=FB_GCS_PATH_TEMPLATE_TIER2
 )
 
 AMZN_CSV_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_AMZN.csv'
-AMZN_PARQUET_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_AMZN_modified.parquet'
+AMZN_PARQUET_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_AMZN.parquet'
 AMZN_GCS_PATH_TEMPLATE_TIER1 = "tier1/AMAZON/data_AMZN.csv"
 AMZN_GCS_PATH_TEMPLATE_TIER2 = "tier2/AMAZON/data_AMZN.parquet"
 
@@ -175,14 +170,14 @@ donwload_parquetize_upload_dag(
     dag=AMZN_data_dag,
     stock_name="AMZN",
     local_csv_path_template=AMZN_CSV_LOCAL_FILE_TEMPLATE,
-    local_parquet_path_template_modfied=AMZN_PARQUET_LOCAL_FILE_TEMPLATE,
+    local_parquet_path_template=AMZN_PARQUET_LOCAL_FILE_TEMPLATE,
     tier1_gcs_path_template=AMZN_GCS_PATH_TEMPLATE_TIER1,
     tier2_gcs_path_template=AMZN_GCS_PATH_TEMPLATE_TIER2
 )
 
 
 AAPL_CSV_LOCAL_FILE_TEMPLATE = AIRFLOW_HOME + '/data_APPLE.csv'
-AAPL_PARQUET_LOCAL_FILE_TEMPLATE_MODIFIED = AIRFLOW_HOME + '/data_APPLE_modified.parquet'
+AAPL_PARQUET_LOCAL_FILE_TEMPLATE_MODIFIED = AIRFLOW_HOME + '/data_APPLE.parquet'
 AAPL_GCS_PATH_TEMPLATE_TIER1 = "tier1/APPLE/data_AAPLE.csv"
 AAPL_GCS_PATH_TEMPLATE_TIER2 = "tier2/APPLE/data_AAPLE.parquet"
 
@@ -200,7 +195,7 @@ donwload_parquetize_upload_dag(
     dag=APPLE_data_dag,
     stock_name="AAPL",
     local_csv_path_template=AAPL_CSV_LOCAL_FILE_TEMPLATE,
-    local_parquet_path_template_modfied=AAPL_PARQUET_LOCAL_FILE_TEMPLATE_MODIFIED,
+    local_parquet_path_template=AAPL_PARQUET_LOCAL_FILE_TEMPLATE_MODIFIED,
     tier1_gcs_path_template=AAPL_GCS_PATH_TEMPLATE_TIER1,
     tier2_gcs_path_template=AAPL_GCS_PATH_TEMPLATE_TIER2
 )
